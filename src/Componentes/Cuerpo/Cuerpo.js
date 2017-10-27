8ba29002e57ba076d597a52c4f5975b7e8f112d7
JoshMR94 <joshuamarram@gmail.com>
2017-10-27 13:07:30 +0100

    Correcciones añadidas

--- src/Componentes/Cuerpo/Cuerpo.js
+++ src/Componentes/Cuerpo/Cuerpo.js
@@ -1,26 +1,17 @@
 import React, { Component } from 'react';
-<<<<<<< HEAD
 import { Switch, Route } from 'react-router-dom';
 import './Cuerpo.css';
 
 
 import MisLibros from './MisLibros/MisLibros';
 
-=======
-import './Cuerpo.css';
-
->>>>>>> 62af7b0eeca4927237537cd9c4560d975d4fb60a
 class Cuerpo extends Component {
   render() {
     return (
       <div id="cuerpo">
-<<<<<<< HEAD
         <Switch>
           <Route exact path='/MisLibros' component={MisLibros}/>
         </Switch>
-=======
-        Cuerpo
->>>>>>> 62af7b0eeca4927237537cd9c4560d975d4fb60a
       </div>
     );
   }
